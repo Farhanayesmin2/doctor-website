ae4ad82288be1bc1873f2044dedb284ef2e2f148
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-11-13 14:39:04 +0600

    Apoinment Added

--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import InfoCards from '../InfoCard/InfoCards';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
 import Testimonial from '../Testimonial/Testimonial';
 import Banner from './Banner/Banner';
@@ -7,9 +8,10 @@ import Banner from './Banner/Banner';
 const Home = () => {
     return (
         <div>
-           <Banner></Banner>
-            <Services></Services>   
+            <Banner></Banner>
             <InfoCards></InfoCards>
+            <Services></Services>   
+        <MakeAppointment></MakeAppointment>
             <Testimonial></Testimonial>
 
         </div>
