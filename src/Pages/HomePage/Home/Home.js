2eb74f713ccae341d2eb2c98fc143e606c344618
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-11-13 14:29:06 +0600

    Testimonial add

--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import InfoCards from '../InfoCard/InfoCards';
 import Services from '../Services/Services';
+import Testimonial from '../Testimonial/Testimonial';
 import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
-
-         <Services></Services>   
-         <InfoCards></InfoCards>
+            <Services></Services>   
+            <InfoCards></InfoCards>
+            <Testimonial></Testimonial>
 
         </div>
     );
